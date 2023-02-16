25fdfbb368614dfdbbe2d962941c8d784c60dab3
AdrianMay200 <124843544+AdrianMay200@users.noreply.github.com>
2023-02-16 12:00:31 -0600

    Update index.js

--- ejemplo/src/index.js
+++ ejemplo/src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+//Aqui se importan el react que es una biblioteca de java y los estilos de la app
 
 import App from "./App";
 
